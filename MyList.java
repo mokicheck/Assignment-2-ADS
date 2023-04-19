d42bb33b10085639e3872e717c23fa2bd8838285
mokicheck <118475936+mokicheck@users.noreply.github.com>
2023-04-20 02:39:42 +0600

    i added an interface MyList

--- MyList.java
+++ MyList.java
@@ -1,2 +1,15 @@
 public interface MyList<E> {
+    int size();
+    boolean contains(Object o);
+    void add(E item);
+    void add(E item, int index);
+    boolean remove(E item);
+    E remove (int index);
+    void clear();
+    E get(int index);
+    int indexOf(Object o);
+    int lastIndexOf(Object o);
+    void sort();
+
+
 }
